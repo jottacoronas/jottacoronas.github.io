fc839c0ac37916479a4fd557c45c96430924ab62
jottacoronas <130712302+jottacoronas@users.noreply.github.com>
2023-11-20 20:30:12 -0300

    Add files via upload

---
+++ aula1.js
@@ -0,0 +1,13 @@
+//window.alert("Olá Mundo!")
+//console.log("teste log")
+//document.write("teste write")
+//document.getElementById("titulo").innerHTML = "<mark>inserindo o js no html</mark"
+
+a = 1
+b = a+2
+
+nome = "joao"
+
+oi = true
+
+alert("meu nome é "+nome)
