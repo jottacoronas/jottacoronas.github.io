406d0dcb7ed512bb9804800f345e57e81722b3c0
jottacoronas <130712302+jottacoronas@users.noreply.github.com>
2023-11-27 20:30:49 -0300

    Add files via upload

--- aula1.js
+++ aula1.js
@@ -3,11 +3,22 @@
 //document.write("teste write")
 //document.getElementById("titulo").innerHTML = "<mark>inserindo o js no html</mark"
 
-a = 1
+/*a = 1
 b = a+2
 
 nome = "joao"
 
 oi = true
 
-alert("meu nome é "+nome)
+alert("meu nome é "+nome)*/
+
+clicks  = 0
+
+
+function teste(){
+    clicks += 1
+    document.getElementById("contador").innerHTML = clicks
+    if(clicks >= 10){
+    document.getElementById("contador").style.backgroundColor = 'red'
+    document.getElementById("contador").style.transition = '4s'
+}}
