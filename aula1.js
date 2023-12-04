5d7a42449c1f7387665880d8c201ad68c03e6f22
jottacoronas <130712302+jottacoronas@users.noreply.github.com>
2023-12-04 20:27:19 -0300

    12/4
    12/4

--- aula1.js
+++ aula1.js
@@ -12,9 +12,22 @@ oi = true
 
 alert("meu nome é "+nome)*/
 
-clicks  = 0
+contagem  = 0
+
+function teste(){
+    valor = parseInt(document.getElementById('valor').value)
+    contagem += valor
+    document.getElementById("resultado").innerHTML = contagem
+
+}
 
 
+
+
+
+
+
+/*
 function teste(){
     clicks += 1
     document.getElementById("contador").innerHTML = clicks
@@ -22,3 +35,26 @@ function teste(){
     document.getElementById("contador").style.backgroundColor = 'red'
     document.getElementById("contador").style.transition = '4s'
 }}
+function teste2(){
+    clicks -= 1
+    document.getElementById("contador").innerHTML = clicks
+    
+}
+
+function teste(){
+    clicks += v
+    document.getElementById('contagem').innerHTML = clicks
+} 
+
+   --------------- HTML-------------------------
+    <button onclick="teste(1)">
+        Aumente
+    </button>
+
+    <label for="" id="contador">0</label>
+
+    <button onclick="teste(-1)">
+        Diminua
+    </button>
+
+*/
